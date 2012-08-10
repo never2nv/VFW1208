193fa2bd72ab59fb14c06f36d00861b0f963c725
never2nv <fngnation@gmail.com>
2012-08-09 21:21:54 -0400

    Unexpected Token
    I randomly ran into this error again, I had literally fixed all errors,
    was writing to localStorage and then this hit. I imagine it's an old
    bad habit resurfacing again; hopefully I can fix this prior to due time
    or that it's a simple early closing of a function or something?
    HOPEFULLY I'm not graded too harshly on this and can learn from the
    mistake and redo it 100 times to remember not to do it again!

--- src/JS/main.js
+++ src/JS/main.js
@@ -30,25 +30,104 @@ window.addEventListener("DOMContentLoaded", function(){
 		selectLi.appendChild(makeSelect);
 	}
 	
+	// Find value of checkbox
+	function getCheckBoxValue(){
+		if ($('favorite').checked){
+			favoriteValue = $('favorite').value;	
+		}else{
+			favoriteValue = "No"
+		 }
+	}
+	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('mainInfo').style.display = "none";
+				$('clearData').style.display = "inline";
+				$('displayLink').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('mainInfo').style.display = "block";
+				$('clearData').style.display = "inline";
+				$('displayLink').style.display = "inline";
+				$('addNew').style.display = "none";
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
 	function storeData(){
-		localStorage.setItem("test", "hello");
-		
+		var id			= Math.floor(Math.random()*100000001);
+		// Gather up all our form field values and store in an object.
+		// Object properties will contain array with form label and input values.
+		getCheckBoxValue();
+		var item		= {};
+		item.relation = ["Relation:", $('relation').value];
+		item.fname    = ["First Name:", $('fname').value];
+		item.mname    = ["Middle Name:", $('mname').value];
+		item.lname    = ["Last Name:", $('lname').value];
+		item.history  = ["History:", $('history').value];
+		item.rating   = ["Rating:", $('rating').value];
+		item.dob      = ["Date of Birth:", $('dob').value];
+		item.favorite	= ["Mark As Favorite:", $('favorite').value];
+		item.honors	= ["Military Honors:", $('favorite').value];
+		// Saving data into local storage Use Stringify to convert object into a string.
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Family Member Saved!");	
+	}
+	
+	function getData(){
+	toggleControls("on");
+		// Write data from local storage to browser.
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0; i<localStorage.length; i++){
+			var makeLi = document.createElement('li');
+			makeList.appendChild(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			// JSON PARSE is opposite of stringify, parsing tunrs vlaue into an object!
+			var makeSubList = document.createElement('ul');
+			makeLi.appendChild(makeSubList);
+			for (var n in obj){
+				var makeSubLi = document.createElement('li');
+				makeSubList.appendChild(makeSubLi);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubLi.innerHTML = optSubText;	
+			}
+		}
+	}
+	
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no relatives to clear!");
+		}else{
+			localStorage.clear();
+			alert("All relatives are deleted!");
+			window.location.reload();
+			return false;
+		}
 	}
 	
 	// Variable defaults
-	var relationCategory = ["--Choose A Family Relation--", "Mother", "Father", "Spouse", "Unknown"];
+	var relationCategory = ["--Choose A Family Relation--", "Mother", "Father", "Spouse", "Unknown"],
+		favoriteValue = "No";
+		
 	makeCats();
 	
 	// Set link & click events
-/*
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
-	var clearLink = $('clear');
+	var clearLink = $('clearData');
 	clearLink.addEventListener("click", clearLocal);
-	*/
 	var save = $('submit');
 	save.addEventListener("click", storeData);
-	
-	
-	
-});
+)
+};
